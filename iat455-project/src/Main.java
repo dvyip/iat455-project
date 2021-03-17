e82e7de321fbec73751ac8309d110bfca54750cb
gayan <gayan@DESKTOP-AP5HDSL>
2021-03-17 15:13:42 -0700

    Added image display and button template

--- iat455-project/src/Main.java
+++ iat455-project/src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FileDialog;
 import java.awt.Image;
@@ -9,52 +10,180 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.Border;
 
 public class Main {
-	private int windowWidth = 1280;
-	private int windowHeight = 720;
+	private int windowWidth = 1920;
+	private int windowHeight = 1000;
 	
 	private JFrame frame;
-	private JButton uploadButton;
-	private JLabel imageDisplay;
+	private JButton hostButton;
+	private JButton donorButton;
+	
+	private JLabel hostDisplay;
+	private JLabel donorDisplay;
+
+	private JPanel buttonPanel = new JPanel();
+	private JPanel previewPanel = new JPanel();
+	private JPanel rightPanel = new JPanel();
+	private JPanel resultPanel = new JPanel();
+	private JPanel adjustmentPanel = new JPanel();
+	
+	private JLabel finalDisplay1;
+	private JLabel finalDisplay2;
+	private JLabel finalDisplay3;
+	
+	private JButton finalButton1;
+	private JButton finalButton2;
+	private JButton finalButton3;
+	private JSlider bitSlider = new JSlider(JSlider.HORIZONTAL, 0, 8, 0);
+	
+	private JButton generateButton;
+
+	//Used to do adjustments 
+	BufferedImage finalImage1;
+	BufferedImage finalImage2;
+	BufferedImage finalImage3;
 	
 	public Main() {
 		frame = new JFrame("Image Displayer");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(windowWidth, windowHeight);
 		
+		hostDisplay = new JLabel();
+		donorDisplay = new JLabel();
+		finalDisplay1 = new JLabel();
+		finalDisplay2 = new JLabel();
+		finalDisplay3 = new JLabel();
 		
-		JPanel buttonPanel = new JPanel();
-		uploadButton = new JButton("Upload");
-		uploadButton.setBounds(0,0,100,50);
-		uploadButton.addActionListener(new ActionListener() {
-
+		
+		hostDisplay.setPreferredSize(new Dimension(windowWidth / 5 - 10, windowHeight - 100));
+		donorDisplay.setPreferredSize(new Dimension(windowWidth / 5 - 10, windowHeight - 100));
+		finalDisplay1.setPreferredSize(new Dimension(windowWidth / 5 - 10, windowHeight / 2 ));
+		finalDisplay2.setPreferredSize(new Dimension(windowWidth / 5 - 10, windowHeight / 2 ));
+		finalDisplay3.setPreferredSize(new Dimension(windowWidth / 5 - 10, windowHeight / 2 ));
+		rightPanel.setPreferredSize(new Dimension(windowWidth/5 * 3, windowHeight));
+		
+		adjustmentPanel.setPreferredSize(new Dimension(windowWidth/5 * 3, windowHeight / 2 - 170));
+		
+		//Temporary border to view bounds
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+		hostDisplay.setBorder(border);
+		donorDisplay.setBorder(border);
+		finalDisplay1.setBorder(border);
+		finalDisplay2.setBorder(border);
+		finalDisplay3.setBorder(border);
+		
+		//initialize slider
+		bitSlider.setMajorTickSpacing(2);
+		bitSlider.setMinorTickSpacing(1);
+		bitSlider.setPaintTicks(true);
+		bitSlider.setPaintLabels(true);
+		
+		//Host Button
+		hostButton = new JButton("Host");
+		hostButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					uploadHostImage();
+					uploadHostImage(hostDisplay);
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-				
+			}
+		});
+		
+		//Donor Button
+		donorButton = new JButton("Donor");
+		donorButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					uploadHostImage(donorDisplay);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		
+		//Generate results
+		generateButton = new JButton("Generate");
+		generateButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				genereateResults();
 			}
 			
 		});
-		imageDisplay = new JLabel();
 		
-		buttonPanel.add(uploadButton);
+		//Download results
+		finalButton1 = new JButton("Alg 1");
+		finalButton2 = new JButton("Alg 2");
+		finalButton3 = new JButton("Alg 3"); 
 		
-		frame.add(BorderLayout.CENTER, imageDisplay);
+		finalButton1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				downloadImage(finalImage1);
+			}
+		});
+		
+		finalButton2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				downloadImage(finalImage2);
+			}
+		});
+		
+		finalButton3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				downloadImage(finalImage3);
+			}
+		});
+		
+		hostDisplay.add(hostButton);
+		donorDisplay.add(donorButton);
+		System.out.println(hostDisplay.getWidth());
+		hostButton.setBounds((int) (hostDisplay.getPreferredSize().getWidth()/2) - 50, (int) hostDisplay.getPreferredSize().getHeight() - 20, 100, 20);
+		donorButton.setBounds((int) (donorDisplay.getPreferredSize().getWidth()/2) - 50, (int) donorDisplay.getPreferredSize().getHeight() - 20, 100, 20);
+		
+		previewPanel.add(hostDisplay);
+		previewPanel.add(donorDisplay);
+		
+		finalDisplay1.add(finalButton1);
+		finalButton1.setBounds((int) (finalDisplay1.getPreferredSize().getWidth()/2) - 50, (int) finalDisplay1.getPreferredSize().getHeight() - 20, 100, 20);
+		finalDisplay2.add(finalButton2);
+		finalButton2.setBounds((int) (finalDisplay2.getPreferredSize().getWidth()/2) - 50, (int) finalDisplay2.getPreferredSize().getHeight() - 20, 100, 20);
+		finalDisplay3.add(finalButton3);
+		finalButton3.setBounds((int) (finalDisplay3.getPreferredSize().getWidth()/2) - 50, (int) finalDisplay3.getPreferredSize().getHeight() - 20, 100, 20);
+		
+		
+		resultPanel.add(finalDisplay1);
+		resultPanel.add(finalDisplay2);
+		resultPanel.add(finalDisplay3);
+		rightPanel.add(BorderLayout.NORTH, resultPanel);
+		
+		adjustmentPanel.add(bitSlider);
+		adjustmentPanel.add(generateButton);
+		
+		rightPanel.add(adjustmentPanel);
+		
+		frame.add(BorderLayout.WEST, previewPanel);
 		frame.add(BorderLayout.SOUTH, buttonPanel);
+		frame.add(BorderLayout.EAST, rightPanel);
 		frame.setVisible(true);
 	}
 	
-	private void uploadHostImage() throws IOException {
+	private void uploadHostImage(JLabel display) throws IOException {
 		FileDialog dialog = new FileDialog(frame, "Select");
 		dialog.setMode(FileDialog.LOAD);
 		dialog.setVisible(true);
@@ -63,19 +192,28 @@ public class Main {
 		
 		BufferedImage image = ImageIO.read(new File(path));
 
-		Dimension newImageDimension = getNewDimensions(new Dimension(image.getWidth(), image.getHeight()));
+		Dimension newImageDimension = getNewDimensions(new Dimension(image.getWidth(), image.getHeight()), new Dimension(display.getWidth(), display.getHeight()));
 
 		ImageIcon imgIcon = new ImageIcon(new ImageIcon(image).getImage().getScaledInstance(newImageDimension.width, newImageDimension.height, Image.SCALE_DEFAULT));
-		imageDisplay.setIcon(imgIcon);
-		imageDisplay.setMaximumSize(new Dimension(1000, 600));
+		display.setIcon(imgIcon);
 		frame.revalidate();
 		frame.repaint();
 	}
 	
-	// return new dimensions to fit canvas
-	Dimension getNewDimensions(Dimension image) {
-	    double widthRatio = windowWidth / image.getWidth();
-	    double heightRatio = windowHeight / image.getHeight();
+	private void downloadImage(BufferedImage image) {
+		//Add code here to download image
+		
+		System.out.println("Download Image");
+	}
+	
+	private void genereateResults() {
+		//Add code here to generate results
+	}
+	
+	// return new dimensions to fit display
+	Dimension getNewDimensions(Dimension image, Dimension display) {
+	    double widthRatio = display.getWidth() / image.getWidth();
+	    double heightRatio = display.getHeight() / image.getHeight();
 	    double ratio = Math.min(widthRatio, heightRatio);
 	    return new Dimension((int) (image.width  * ratio), (int) (image.height * ratio));
 	}
